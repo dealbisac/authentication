a75ff9cadd5b6024cf03439728dddf1ddf2559c9
dealbisac <chand.dipendra19@gmail.com>
2024-01-22 12:37:09 +0100

    add templates file for landing page

--- auth/home/views.py
+++ auth/home/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello from home view")
+    return render(request, 'index.html')
 
+def signup(request):
+    return HttpResponse("Hello from signup view")
