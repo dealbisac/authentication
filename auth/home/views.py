408bb430c97eb97552d98dc890949844a5a22828
dealbisac <chand.dipendra19@gmail.com>
2024-01-24 13:17:36 +0100

    add login form and update home urls && views

--- auth/home/views.py
+++ auth/home/views.py
@@ -6,5 +6,24 @@ def home(request):
     context = {'page' : 'Home'}
     return render(request, 'index.html', context)
 
-def signup(request):
+def signup_page(request):
     return HttpResponse("Hello from signup view")
+
+def login_page(request):
+    context = {'page' : 'Login'}
+    return render(request, 'login.html', context)
+
+def logout_page(request):
+    return HttpResponse("Hello from logout view")
+
+def profile_page(request):
+    return HttpResponse("Hello from profile view")
+
+def edit_profile_page(request):
+    return HttpResponse("Hello from edit profile view")
+
+def forgot_password_page(request):
+    return HttpResponse("Hello from forgot password view")
+
+def reset_password_page(request):
+    return HttpResponse("Hello from reset password view")
