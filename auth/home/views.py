188df3b2d1071663d9bdc285782b2a26a5573d04
dealbisac <chand.dipendra19@gmail.com>
2024-01-24 16:18:14 +0100

    add login logic for user

--- auth/home/views.py
+++ auth/home/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
+# Home page view
 def home(request):
     context = {'page' : 'Home'}
     return render(request, 'index.html', context)
 
 
+# Sign up page view
 def signup_page(request):
     context = {'page' : 'Sign Up'}
 
@@ -43,12 +46,33 @@ def signup_page(request):
     return render(request, 'signup.html', context)
 
 
+# Sign in (Login) page view
 def login_page(request):
     context = {'page' : 'Sign'}
+
+    # data from form
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # check if email or username already exists
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                messages.success(request, "Login successful.")
+                return redirect('/profile/')
+            else:
+                messages.error(request, "Invalid password.")
+                return redirect('/login/')
+        else:
+            messages.error(request, "Email does not exist.")
+            return redirect('/login/')
     return render(request, 'signin.html', context)
 
+# Logout page view
 def logout_page(request):
-    return HttpResponse("Hello from logout view")
+    #logout(request)
+    return redirect('/login/')
 
 def profile_page(request):
     return HttpResponse("Hello from profile view")
