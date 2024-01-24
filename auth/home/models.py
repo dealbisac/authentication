0be2c79cbb34438ec368548d5548db66f710f815
dealbisac <chand.dipendra19@gmail.com>
2024-01-24 14:44:25 +0100

    fix: update only necessary fields in database

--- auth/home/models.py
+++ auth/home/models.py
@@ -3,10 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     email = models.EmailField()
-    username = models.CharField(max_length=100)
     anonymousname = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- auth/home/views.py
+++ auth/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,21 @@ def home(request):
 
 def signup_page(request):
     context = {'page' : 'Sign Up'}
+
+    # data from form
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        anonymousname = request.POST.get('anonymousname')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        avatar = request.FILES.get('avatar')
+
+        # save data to database
+        user = User.objects.create_user(email=email, username=username, password=password)
+        user.save()
+
+        # return to home page
+        return HttpResponse("Hello from signup view")
     return render(request, 'register.html', context)
 
 def login_page(request):
