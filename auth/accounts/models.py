98c1a53bfb92b66ce9c8871d74f0bd593478cb78
dealbisac <chand.dipendra19@gmail.com>
2024-01-25 02:16:56 +0100

    define new customusermanager

--- auth/accounts/models.py
+++ auth/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin, UserManager
 
 # Create your models here.
 
@@ -13,4 +13,72 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'password']
+
+"""
+class CustomUserManager(UserManager):
+    def _create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError("Email is required.")
+        if not username:
+            raise ValueError("Username is required.")
+        if not password:
+            raise ValueError("Password is required.")
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email=None, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(email, username, password, **extra_fields)
+    
+    def create_superuser(self, email=None, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self._create_user(email, username, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    user_image = models.ImageField(upload_to='user_images/', blank=True)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+"""
+
+class Meta:
+    verbose_name = 'User'
+    verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email
+    
+    def get_full_name(self):
+        return self.name or self.username
+    
+    def get_short_name(self):
+        return self.name or self.email.split('@')[0] or self.username
+    
+    
+    
+
 
