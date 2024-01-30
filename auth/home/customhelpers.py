cb062dc60631b4117204f0174a4529237e2c5f96
dealbisac <chand.dipendra19@gmail.com>
2024-01-30 01:21:07 +0100

    add helper function to generate random strings

---
+++ auth/home/customhelpers.py
@@ -0,0 +1,18 @@
+# File to put the custom helper function here.
+
+# A function to generate the random string
+import random
+import string
+
+
+def get_string(letters_count, digits_count):
+    letters = ''.join((random.choice(string.ascii_letters) for i in range(letters_count)))
+    digits = ''.join((random.choice(string.digits) for i in range(digits_count)))
+
+    # Convert resultant string to list and shuffle it to mix letters and digits
+    sample_list = list(letters + digits)
+    random.shuffle(sample_list)
+    # convert list to string
+    final_string = ''.join(sample_list)
+    return final_string
+
